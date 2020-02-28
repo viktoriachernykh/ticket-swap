e9d9a86c5a1e5946444650ae6aa8197a626173cc
viktoriachernykh <chernycv@gmail.com>
2020-02-28 08:39:23 +0100

    Unnecessary comments deleted

--- server/index.js
+++ server/index.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-// const Sse = require("json-sse"); // for stream
-// const Channel = require("./channel/model"); // for stream
-// const Message = require("./message/model"); // for stream
-// const messageFactory = require("./message/router"); // for stream
-// const channelFactory = require("./channel/router"); // for stream
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -24,29 +19,6 @@ app.use(corsMiddleware);
 
 const jsonMiddleware = express.json();
 app.use(jsonMiddleware);
-
-// STREAM
-// const stream = new Sse();
-// app.get("/stream", async (req, res, next) => {
-//   try {
-//     const tickets = await Ticket.findAll({ include: [Risk] });
-//     const action = {
-//       type: "ALL_RATINGS",
-//       tickets
-//     };
-//     const json = JSON.stringify(action);
-//     stream.updateInit(json);
-//     stream.init(req, res);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// const messageRouter = messageFactory(stream);
-// app.use(messageRouter);
-
-// const channelRouter = channelFactory(stream);
-// app.use(channelRouter);
 
 app.use(userRouter, eventRouter, ticketRouter, commentRouter, authRouter);
 
